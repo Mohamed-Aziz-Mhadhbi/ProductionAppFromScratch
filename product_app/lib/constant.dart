029781d53fd7a1f0f2e7892f93b46de7e8c63672
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2025-02-12 11:45:52 +0100

    changing to firebase

--- product_app/lib/constant.dart
+++
@@ -1,2 +0,0 @@
-const MONGO_URL = "mongodb+srv://user:test@todolist.k7irg.mongodb.net/?retryWrites=true&w=majority&appName=TodoList";
-const COLLECTION_NAME = "user";
--- product_app/lib/main.dart
+++ product_app/lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:product_app/mongodb.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await MongoDatabase.connect();
   runApp(const MyApp());
 }
 
